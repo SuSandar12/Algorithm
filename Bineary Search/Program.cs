e3f94e16f7aa67e00dd043ae325a6d9e425a1d53
Hsu Sandar Swe <susandarswe.mm@gmail.com>
2019-11-03 16:03:52 +0630

    Binary search and Linear Search

---
+++ Bineary Search/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bineary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter array (eg: 1,2) : ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Value to Search : ");
+            int value = Console.Read();
+
+            string[] strArray = str.Split(',');
+            int[] array = new int[strArray.Length]; 
+
+            for(int i=0; i< strArray.Length; i++)
+            {
+                array[i] = Convert.ToInt32(strArray[i]);
+            }
+
+
+            Console.WriteLine(SearchByBinary(array, value));
+            Console.ReadLine();
+        }
+
+        private static string SearchByBinary(int[] array, int value)
+        {
+            string result = "value not found";
+            int lower = 1;
+            int upper = array.Length;
+
+            //O(LogN)
+            while (lower <= upper)
+            {
+                int midpoint = (lower + upper) / 2;
+                if (array[midpoint] > value)
+                {
+                    upper = midpoint - 1;
+                }
+                else if (array[midpoint] < value)
+                {
+                    lower = midpoint + 1;
+                }
+                else
+                {
+                    result = "Value found in " + midpoint;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Linear Search/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter array (eg: 1,2) : ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Value to Search : ");
+            string value = Console.ReadLine();
+
+            string[] array = str.Split(',');
+
+            Console.WriteLine(SearchByLinear(array, value));
+            Console.ReadLine();
+
+        }
+
+        public object[] StringToArray(string input, string separator, Type type)
+        {
+            string[] stringList = input.Split(separator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            Object[] list = new object[stringList.Length];
+
+            for (int i = 0; i < stringList.Length; i++)
+            {
+                list[i] = Convert.ChangeType(stringList[i], type);
+            }
+            return list;
+        }
+
+        public static string SearchByLinear(string[] array, string value)
+        {
+            string result = "value not found";
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                if(value == array[j])
+                {
+                    result += "Value found in array index : " + j;
+                }
+            }
+
+            return result;
+        }
+
+        
+    }
+}
